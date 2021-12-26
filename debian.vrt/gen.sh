eefc3c79d6576e23f7f916509413ab7d807d8ba5
Michael Nutt <michael@nuttnet.net>
2021-12-26 11:34:21 -0500

    add deb files from libvmod-xcounter

---
+++ debian.vrt/gen.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/140/"   > 140/changelog
+cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/130/"   > 130/changelog
+cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/999/"   > trunk/changelog
---
+++ debian.vrt/pkg.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+CN=`lsb_release -cs`
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/140/"   > 140/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/130/"   > 130/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/999/"   > trunk/changelog
