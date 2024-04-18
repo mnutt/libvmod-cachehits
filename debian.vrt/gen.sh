91a93e485058717832bab12077a05773092474c9
Michael Nutt <michael@nuttnet.net>
2024-04-18 08:53:47 -0400

    support varnish 7.5

--- debian.vrt/gen.sh
+++ debian.vrt/gen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/190/"   > 190/changelog
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/140/"   > 140/changelog
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/130/"   > 130/changelog
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/71/"    > 71/changelog
