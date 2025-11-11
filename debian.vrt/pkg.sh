f91df7f360fe83579de1ed6be05359c9f83fc7c3
Michael Nutt <michael@nuttnet.net>
2025-11-11 11:12:32 -0500

    remove VRT 130 (varnish 6.6)

--- debian.vrt/pkg.sh
+++ debian.vrt/pkg.sh
@@ -2,6 +2,5 @@
 CN=`lsb_release -cs`
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/220/"   > 220/changelog
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/190/"   > 190/changelog
-cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/130/"   > 130/changelog
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/71/"    > 71/changelog
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/999/"   > trunk/changelog
