b0504958557245de693e6e248940f016ec1a900e
Michael Nutt <michael@nuttnet.net>
2025-11-16 16:44:23 -0500

    try better versioning setup

--- debian.vrt/pkg.sh
+++ debian.vrt/pkg.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 CN=`lsb_release -cs`
-cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/220/"   > 220/changelog
-cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/190/"   > 190/changelog
-cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/71/"    > 71/changelog
-cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/999/"   > trunk/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VVER%/varnish60/" | sed -r "s/%DIST%/$CN/" > varnish60/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VVER%/varnish77/" | sed -r "s/%DIST%/$CN/" > varnish77/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VVER%/varnish80/" | sed -r "s/%DIST%/$CN/" > varnish80/changelog
--- sw_vrt.sh
+++ sw_vrt.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
-if [ -d debian.vrt/$1 ]; then
-  ln -nfs debian.vrt/$1 debian
+VARNISH_DIR="varnish$1"
+if [ -d debian.vrt/$VARNISH_DIR ]; then
+  ln -nfs debian.vrt/$VARNISH_DIR debian
 else
-  echo "Not support VRT=$1"
+  echo "Does not support varnish$1"
 fi
 
